e61ab1b8c9fb5cbdb39b42da6a4abc4f75631669
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-11-10 17:58:29 +0900

    feat: project init

---
+++ src/main/java/ku/ssa/stockanalyzerrag/StockAnalyzerRagApplication.java
@@ -0,0 +1,13 @@
+package ku.ssa.stockanalyzerrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyzerRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAnalyzerRagApplication.class, args);
+    }
+
+}
